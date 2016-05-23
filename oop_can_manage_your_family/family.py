80658e12a877104975e25679584c9a3f57a68bfc
Gloria Adams <gloriaadams@Glorias-iMac.local>
2016-05-23 00:51:15 -0400

    Task 3 & 4 Attempted

--- oop_can_manage_your_family/family.py
+++ oop_can_manage_your_family/family.py
@@ -13,6 +13,9 @@ Task 5:
 Gloria Bwandungi, 2016.
 '''
 
+import json
+import os.path
+
 class Person(object):
 
     '''Class Attributes'''
@@ -47,7 +50,48 @@ class Person(object):
             raise Exception("eyes_color is not valid")
         self.__eyes_color = eyes_color
 
+    '''  Public Attributes '''
+    def is_married_to(int):
+        self.is_married_to = is_married_to
 
+    ''' Public Methods'''
+    def can_run(self):
+        return False
+
+    def need_help(self):
+        return True
+
+    def is_young(self):
+        return True
+
+    def can_vote(self):
+        return False
+
+    def can_be_married(self):
+        return False
+
+    def is_married(self):
+        if self.is_married_to == 0:
+            return False
+        else:
+            return True
+
+    def divorce(self, p):
+        self.is_married_to = 0
+        p.is_married_to = 0
+
+
+    def just_married_with(self, p):
+        if self.can_be_married() or p.can_be_married() == False:
+            raise Exception("Can't be married")
+        if self.is_married_to != 0 or p.is_married_to != 0:
+            raise Exception("Already married")
+        self.is_married_to = p.__id
+        p.is_married_to = self.__id
+        if self.__genre == "Male":
+            p.last_name = self.last_name
+        if p.__genre == "Male":
+            self.last_name = p.last_name
 
     ''' Getters '''
     def get_id(self):
@@ -66,7 +110,6 @@ class Person(object):
         return self.__genre
 
     def __str__(self):
-        
         ''' Returns full name '''
         return str(self.__first_name  + " " + self.last_name)
 
@@ -103,3 +146,120 @@ class Person(object):
 
     def __ne__(self,other):
         return self.age() != other.age()
+
+    '''The JSON Method'''
+    def json(self):
+        json = {
+            'id': self.__id,
+            'eyes_color': self.__eyes_color,
+            'genre': self.__genre,
+            'date_of_birth': self.__date_of_birth,
+            'first_name': self.__first_name,
+            'last_name': self.last_name,
+            'is_married_to': self.is_married_to
+        }
+
+    '''Sets Person attributes by value from the dictionary(Hash)'''
+    def load_from_json(self, json):
+        if type(json) != dict:
+            raise Exception("json is not valid")
+
+        self.__id = json['id']
+        self.__eyes_color = json['eyes_color']
+        self.__genre = json['genre']
+        self.__date_of_birth = json['date_of_birth']
+        self.__first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.is_married_to = json['is_married_to']
+
+
+
+
+''' Subclasses Defined '''
+class Baby(Person):
+    '''Public Methods'''
+    def can_run(self):
+        return False
+
+    def need_help(self):
+        return True
+
+    def is_young(self):
+        return True
+
+    def can_vote(self):
+        return False
+
+    def can_be_married(self):
+        return False
+
+
+class Teenager(Person):
+    '''Public Methods'''
+    def can_run(self):
+        return True
+
+    def need_help(self):
+        return False
+
+    def is_young(self):
+        return True
+
+    def can_vote(self):
+        return False
+
+    def can_be_married(self):
+        return False
+
+class Adult(Person):
+    '''Public Methods'''
+    def can_run(self):
+        return True
+
+    def need_help(self):
+        return False
+
+    def is_young(self):
+        return False
+
+    def can_vote(self):
+        return True
+
+    def can_be_married(self):
+        return True
+
+
+class Senior(Person):
+    '''Public Methods'''
+    def can_run(self):
+        return False
+
+    def need_help(self):
+        return True
+
+    def is_young(self):
+        return False
+
+    def can_vote(self):
+        return True
+
+    def can_be_married(self):
+        return True
+
+
+''' Takes a list and saves it to a JSON file'''
+def save_to_file(list, filename):
+    if os.path.exists(filename) == False or type(filename) is not str:
+        raise Exception('Filename does not exist')
+    else:
+        with open(filename, 'w') as outfile:
+            json.dump(list, outfile)
+
+''' Takes a JSON file and parses the information into a list'''
+def load_from_file(filename):
+    if os.path.exists(filename) == False or type(filename) is not str:
+        raise Exception('Filename does not exist')
+    else:
+        with open(filename, 'r') as json_data:
+            data = json.load(json_data)
+        return data
