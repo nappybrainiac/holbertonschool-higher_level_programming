72efc19c8aa0a02f56a59da0f9b7bb3bfec3d4c1
Gloria Adams <gloriaadams@Glorias-iMac.local>
2016-05-25 10:12:37 -0400

    Task 4 Complete

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -11,8 +11,15 @@
 
  */
 
-protocol Classify{
+ protocol Classify{
     func isStudent() -> Bool
+}
+
+enum Subject {
+    case Math
+    case English
+    case French
+    case History
 }
 
 class Person {
@@ -30,9 +37,41 @@ class Person {
     func fullName() -> String{
         return first_name + " " + last_name
     }
+
+    func className() -> String {
+        return "Person"
+    }
 }
 
 class Mentor: Person, Classify {
+
+    override func className () -> String {
+		return "Mentor"
+    }
+
+    let subject: Subject
+
+
+    init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+        self.subject = subject
+        super.init(first_name: first_name, last_name: last_name, age: age)
+    }
+
+    func stringSubject() -> String {
+        if self.subject == Subject.Math{
+            return "Math"
+        }
+        if self.subject == Subject.English{
+            return "English"
+        }
+        if self.subject == Subject.French{
+            return "French"
+        }
+        if self.subject == Subject.History{
+            return "History"
+        }
+    return "String"
+    }
 
     func isStudent() -> Bool{
         return false
@@ -42,8 +81,62 @@ class Mentor: Person, Classify {
 
 class Student: Person, Classify {
 
+    override func className () -> String {
+		return "Student"
+    }
+
     func isStudent() -> Bool{
         return true
     }
 
+}
+
+
+class School {
+
+	var name: String
+	var list_persons: [Person]
+
+
+	init(name: String) {
+		self.name = name
+		self.list_persons = []
+	}
+
+
+	func addStudent(p: Person) -> Bool {
+		if p.className() == "Student" {
+			self.list_persons.append(p)
+			return true
+		}
+		return false
+	}
+
+	func addMentor(p: Person) -> Bool {
+		if p.className() == "Mentor" {
+			self.list_persons.append(p)
+			return true
+		}
+		return false
+	}
+
+    func listStudents() -> [Person] {
+        var students = self.list_persons.filter {($0 is Student)}
+        students.sortInPlace {($0.age > $1.age)}
+        return students
+    }
+
+    func listMentors() -> [Person] {
+        var mentors = self.list_persons.filter {($0 is Mentor)}
+        mentors.sortInPlace {($0.age > $1.age)}
+        return mentors
+    }
+
+    func listMentorsBySubject(subject: Subject) -> [Person] {
+        let mentors = self.listMentors() as! [Mentor]
+        let mentorsBySubject: [Mentor] = mentors.filter { (mentor: Mentor) -> Bool in
+            mentor.subject == subject
+        }
+        return mentorsBySubject
+    }
 }
