10599642514e3a4e482420b9868209bc568aa29e
Gloria Adams <gloriaadams@Glorias-iMac.local>
2016-06-05 13:35:37 -0400

    Task 2 Complete

---
+++ run_your_first_ios_app/tapper/tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper
+//
+//  Created by Gloria Adams on 2016-05-26.
+//  Copyright © 2016 Gloria Adams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
