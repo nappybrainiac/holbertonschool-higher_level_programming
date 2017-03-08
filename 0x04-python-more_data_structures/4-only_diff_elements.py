01077df95866fbd12c0702e79490f3037e5252cd
Gloria Bwandungi <30@holbertonschool.com>
2017-03-08 14:59:56 -0500

    Task 4 Complete & ready for checks

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return [i for i in set_1 if i not in set_2]
+    if set_1 is None:
+        return None
+    elif set_2 is None:
+        return None
+    else:
+        return [i for i in set_1 if i not in set_2]
