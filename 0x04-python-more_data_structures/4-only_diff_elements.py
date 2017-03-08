e8fccc04d281d39cff5b42f550b9487dd0e3ab1c
Gloria Bwandungi <30@holbertonschool.com>
2017-03-08 16:11:37 -0500

    Task 4 Complete & ready for checks

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-        return [i for i in set_1 if i not in set_2]
+        unity = set(set_1).union(set(set_2))
+        intersect = set(set_1).intersection(set(set_2))
+        return list(unity - intersect)
