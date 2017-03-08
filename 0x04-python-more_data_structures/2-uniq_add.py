2d27c039c3b15d0d7a316e9a045f49b2cdcd4394
Gloria Bwandungi <30@holbertonschool.com>
2017-03-08 14:01:00 -0500

    Task 2 Complete, ready for checks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(mylist)
+    #for an empty list
+    if my_list is None:
+        return(None)
+
+    #to create a new list of unique characters
+    new_list = set(my_list)
     return sum(new_list)
