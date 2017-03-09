35a1a8fc15699d4951176430429654129102b5b3
Gloria Bwandungi <30@holbertonschool.com>
2017-03-09 15:11:09 -0500

    Task 8 Completed, checking...

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(my_dict, key=""):
-    if my_dict is None:
-        return None
-    elif key in my_dict:
+    if key in my_dict:
         del my_dict[key]
-    else:
-        return None
-    return my_dict            
+    return my_dict
