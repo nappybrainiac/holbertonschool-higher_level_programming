3ffb05168ace4de4a6e8b5d1c81990d8b94da1fe
Gloria Bwandungi <30@holbertonschool.com>
2017-03-09 13:20:22 -0500

    Task 6 Completed, Checking...

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bash/python3
 def print_sorted_dictionary(my_dict):
-    for key in sorted(my_dict.iterkeys()):
-        #print '%s: %s' % (key, my_dict[key])
-        print("{}: {}".format(key, value))
+    for key in sorted(my_dict):
+        print '%s: %s' % (key, my_dict[key])
+        #print("{}: {}".format(key, value))
