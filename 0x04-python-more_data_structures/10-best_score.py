ea50ace1450a5fee1cade5ac21376261cb9c8896
Gloria Bwandungi <30@holbertonschool.com>
2017-03-10 00:32:38 -0500

    Task 10 Complete, checking ...

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(my_dict):
     if my_dict is None:
         return None
     else:
-        maximum = max(my_dict, key=my_dict.get)
-    return maximum
+        return maximum = max(my_dict, key=my_dict.get)
+    #return maximum
