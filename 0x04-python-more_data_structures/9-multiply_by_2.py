1e28879af3841cf563e0576017dc8789a1b9e213
Gloria Bwandungi <30@holbertonschool.com>
2017-03-09 22:28:07 -0500

    Task 9 Complete, checking ...

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    new_dict = my_dict.copy()
-    for value in new_dict:
-        value *= 2
+    if my_dict is None:
+        return None
+    else:
+        new_dict = my_dict.copy()
+        for value in new_dict:
+            value *= 2
     return new_dict
