54ca30a50fe368a0abbfb2cd8f2161eb6f5f3d38
Gloria Bwandungi <30@holbertonschool.com>
2017-03-16 14:18:41 -0400

    Task 1 Complete, checking ....

---
+++ 0x07-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This file contains one class.
+"""
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        This function initializes an instance of square
+        with a size.
+        """
+        self.__size = size
