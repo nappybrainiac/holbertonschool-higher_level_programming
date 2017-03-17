2a78ab270c91fa4092aba8bf194840a88ec57372
Gloria Bwandungi <30@holbertonschool.com>
2017-03-17 09:19:13 -0400

    Task 2 Complete, checking ...

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -10,6 +10,7 @@ class Square:
 
     def __init__(self, size=0):
         """ Initialization function"""
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
