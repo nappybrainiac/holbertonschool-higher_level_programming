130adc932c2b955de83beb68c9ab5bb9b845ce4e
Gloria Bwandungi <30@holbertonschool.com>
2017-03-18 15:11:53 -0400

    __init__ edited to include error handling for position

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -25,6 +25,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if position[0] < 0 or position[1] < 0:
             raise ValueError("position must be a tuple of 2 positive integers")
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
