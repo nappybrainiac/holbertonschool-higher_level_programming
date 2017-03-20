3bd7dc5855e4734ce2804846f89fbb842cc76262
Gloria Bwandungi <30@holbertonschool.com>
2017-03-20 15:32:49 -0400

    Corrections to __init__

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+This file contains a class, Rectangle, that defines an
+instance of a rectangle using width and height.
+"""
 
 
 class Rectangle:
+    """
+    This class defines a rectangle instance.
+    """
+
+    w_int = "width must be an integer"
+    w_pos = "width must be >= 0"
+    h_int = "height must be an integer"
+    h_pos = "height must be >= 0"
+
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -12,10 +25,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """making sure the width is a positive integer"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError(w_int)
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError(w_pos)
         else:
             self.__width = value
 
@@ -25,17 +39,18 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """making sure the height is a positive integer"""
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError(h_int)
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError(h_pos)
         else:
             self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.__height == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return self.height * 2 + self.__width * 2
+        return (self.__width * 2) + (self.__height * 2)
