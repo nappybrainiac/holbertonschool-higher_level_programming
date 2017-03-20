69b80671e0526a738544c6404577bef12aaf5848
Gloria Bwandungi <30@holbertonschool.com>
2017-03-20 15:01:20 -0400

    Rearranged errors to remove red check...

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+This file contains a class, Rectangle, that defines an
+instance of a rectangle using width and height.
+"""
 
 
 class Rectangle:
+    """
+    This class defines a rectangle instance.
+    """
+
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -12,12 +20,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """making sure the width is a positive integer"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -25,9 +33,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """making sure the height is a positive integer"""
         if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+                raise TypeError("height must be an integer")
+        if value < 0:
+                raise ValueError("height must be >= 0")
+        self.__height = value
