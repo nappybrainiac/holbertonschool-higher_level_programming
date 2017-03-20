a945316445b4b46be1dc6335a783e7897a39d1bf
Gloria Bwandungi <30@holbertonschool.com>
2017-03-20 15:15:10 -0400

    Corrections to __init__

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,6 +11,14 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >=0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >=0")            
         self.height = height
         self.width = width
 
@@ -41,9 +49,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             self.__perimeter = 0
-        return (self.__width * 2) + (self.__height * 2)
+        return ((self.__width * 2) + (self.__height * 2))
