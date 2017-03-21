4a94569d4146ed34a3c42be266d3dd09588c778d
Gloria Bwandungi <30@holbertonschool.com>
2017-03-21 14:41:59 -0400

    print removed

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,5 +54,5 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         for i in range(self.__height):
-            print ("#" * self.__width)
-        return ""
+            line = ("#" * self.__width)
+        return line
