b627290dddf50a866cde8c35a0083f36bcd8e3f4
Gloria Bwandungi <30@holbertonschool.com>
2017-03-22 00:43:57 -0400

    main file documentation changed

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
         return ("Rectangle(%s, %s)" % (str(self.__width), str(self.__height)))
 
     def __del__(self):
-        print ("Bye rectangle...")
+        print("Bye rectangle...")
